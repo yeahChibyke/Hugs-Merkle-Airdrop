02dafa69944b00bf6fa7cff6a2e3231eac009009
yeahChibyke <chibykepride@gmail.com>
2024-08-03 16:28:31 +0100

    installed murky. Merkle Tree Script comming up

---
+++ test/TestHugsMerkleAirdrop.t.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.24;
+
+import {HugsMerkleAirdrop} from "../src/HugsMerkleAirdrop.sol";
+import {Test, console2} from "forge-std/Test.sol";
+
+contract TestHugsMerkleAirdrop is Test {}
