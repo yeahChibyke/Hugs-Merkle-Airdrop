f14bc548f4ba1eaf4eb5d6c92fdf1742166065eb
yeahChibyke <chibykepride@gmail.com>
2024-09-07 08:59:13 +0100

    implemented signatures

--- src/HugsAirdrop.sol
+++ src/HugsAirdrop.sol
@@ -4,13 +4,16 @@ pragma solidity ^0.8.24;
 import {HugsToken} from "./HugsToken.sol";
 import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 /**
  * @title Hugs Airdrop
  * @author Chukwubuike Victory Chime
  * @dev This contract handles the distribution of Hugs tokens via a Merkle Tree-based airdrop
  */
-contract HugsAirdrop {
+contract HugsAirdrop is EIP712, ReentrancyGuard {
     using SafeERC20 for IERC20;
 
     // >----------> ERRORS
@@ -18,6 +21,13 @@ contract HugsAirdrop {
     error HugsAirdrop__InvalidProof();
     /// @dev thrown when an account wants to claim Hugs tokens more than once
     error HugsAirdrop__AlreadyClaimedHugs();
+    error HugsAirdrop__SignatureInvalid();
+
+    // >----------> TYPE DECLARATION
+    struct AirdropClaim {
+        address account;
+        uint256 amount;
+    }
 
     // >----------> VARIABLES
     /// @dev array to store addresses of claimers
@@ -31,6 +41,8 @@ contract HugsAirdrop {
     /// @dev mapping to track which addresses have already claimed their airdrop
     mapping(address => bool) private s_hasClaimedHugs;
 
+    bytes32 private constant MESSAGE_TYPEHASH = keccak256("AirdropClaim(address account, uint256 amount)");
+
     // >----------> EVENTS
     /**
      * @dev emitted when a user successfuly claims Hugs tokens
@@ -40,7 +52,7 @@ contract HugsAirdrop {
     event HugClaimed(address indexed claimer, uint256 indexed amount);
 
     // >----------> CONSTRUCTOR
-    constructor(bytes32 merkleRoot, IERC20 airdropToken) {
+    constructor(bytes32 merkleRoot, IERC20 airdropToken) EIP712("HugsAirdrop", "1") {
         i_merkleRoot = merkleRoot;
         i_airdropToken = airdropToken;
     }
@@ -53,9 +65,17 @@ contract HugsAirdrop {
      * @param amount amount of tokens to claim
      * @param merkleProof Merkle proof to validate the claim
      */
-    function claimHugs(address account, uint256 amount, bytes32[] calldata merkleProof) external {
+    function claimHugs(address account, uint256 amount, bytes32[] calldata merkleProof, uint8 v, bytes32 r, bytes32 s)
+        external
+        nonReentrant
+    {
         if (s_hasClaimedHugs[account]) {
             revert HugsAirdrop__AlreadyClaimedHugs();
+        }
+
+        // check signature validity
+        if (!_isValidSignature(account, getMessage(account, amount), v, r, s)) {
+            revert HugsAirdrop__SignatureInvalid();
         }
 
         bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account, amount))));
@@ -71,7 +91,22 @@ contract HugsAirdrop {
         i_airdropToken.safeTransfer(account, amount);
     }
 
+    // >----------> INTERNAL FUNCTIONS
+    function _isValidSignature(address account, bytes32 digest, uint8 v, bytes32 r, bytes32 s)
+        internal
+        pure
+        returns (bool)
+    {
+        (address actualSigner,,) = ECDSA.tryRecover(digest, v, r, s);
+        return actualSigner == account;
+    }
+
     // >>---------------------->> EXTERNAL & PUBLIC VIEW FUNCTIONS
+    function getMessage(address account, uint256 amount) public view returns (bytes32) {
+        return
+            _hashTypedDataV4(keccak256(abi.encode(MESSAGE_TYPEHASH, AirdropClaim({account: account, amount: amount}))));
+    }
+
     /// @dev returns the Merkle root used for the airdrop
     function getMerkleRoot() external view returns (bytes32) {
         return i_merkleRoot;
