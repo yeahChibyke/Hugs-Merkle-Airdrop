c943b17f732f676549faebb2b30cea7d18c2c320
yeahChibyke <chibykepride@gmail.com>
2024-09-11 02:44:24 +0100

    added new tests. re-installed OZ lib. need to confirm a bug with Dalu laptop

--- test/TestHugsMerkleAirdrop.t.sol
+++ test/TestHugsMerkleAirdrop.t.sol
@@ -13,15 +13,22 @@ contract TestHugsAirdrop is Test {
 
     bytes32 ROOT = 0xb1155292a7f465f73a70be41a25a6d7ab8e760cabb68c08072856aa05eb23e32;
 
-    bytes32 proofOne = 0x960bfc5afe0572f554d1ccc4b594f1f4c5aeccab6bf6cc1d2ca735a70078b61d;
-    bytes32 proofTwo = 0x543793e984215b0a24b11f843eea8755b11141397ef712e5af3111067a3019a8;
+    // ------Proofs------ //
+    bytes32 proofA1 = 0x960bfc5afe0572f554d1ccc4b594f1f4c5aeccab6bf6cc1d2ca735a70078b61d;
+    bytes32 proofA2 = 0x543793e984215b0a24b11f843eea8755b11141397ef712e5af3111067a3019a8;
+    bytes32[] aliceProof = [proofA1, proofA2];
 
-    bytes32[] PROOF = [proofOne, proofTwo];
+    bytes32 proofB1 = 0xb4ad04bfcc1a89a0939f16e689fd17d2435174d8431a765e538b306bebd0438e;
+    bytes32 proofB2 = 0x543793e984215b0a24b11f843eea8755b11141397ef712e5af3111067a3019a8;
+    bytes32[] bobProof = [proofB1, proofB2];
 
-    // -------------------------------- //
-    bytes32 proofA = 0xb4ad04bfcc1a89a0939f16e689fd17d2435174d8431a765e538b306bebd0438e;
-    bytes32 proofB = 0x543793e984215b0a24b11f843eea8755b11141397ef712e5af3111067a3019a8;
-    bytes32[] bobProof = [proofA, proofB];
+    bytes32 proofC1 = 0xcc519ead7d2f4eacc153fd8cfade41d33f0fa718fbe454eb2663ae65b2aa6476;
+    bytes32 proofC2 = 0xfe8afbaa6b6838916e80bd2f0d376704cfd04a92db59cc177c9b465249678402;
+    bytes32[] claraProof = [proofC1, proofC2];
+
+    bytes32 proofD1 = 0x03cb497476aa2616a9542659cef0a92c84e884732e6db563047aa3f0ae9ecd69;
+    bytes32 proofD2 = 0xfe8afbaa6b6838916e80bd2f0d376704cfd04a92db59cc177c9b465249678402;
+    bytes32[] danProof = [proofD1, proofD2];
 
     uint256 constant CLAIM_AMOUNT = 25e18;
     uint256 constant SEND_AMOUNT = 100e18;
@@ -63,7 +70,7 @@ contract TestHugsAirdrop is Test {
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(alicePrvKey, digest);
 
         vm.prank(gasPayer);
-        airdrop.claimHugs(Alice, CLAIM_AMOUNT, PROOF, v, r, s);
+        airdrop.claimHugs(Alice, CLAIM_AMOUNT, aliceProof, v, r, s);
 
         uint256 currentBalOfAlice = token.balanceOf(Alice);
         uint256 currentTokenInAIrdrop = token.balanceOf(address(airdrop));
@@ -85,57 +92,83 @@ contract TestHugsAirdrop is Test {
 
         vm.prank(gasPayer);
         vm.expectRevert(HugsAirdrop.HugsAirdrop__SignatureInvalid.selector);
-        airdrop.claimHugs(Alice, CLAIM_AMOUNT, PROOF, v, r, s);
+        airdrop.claimHugs(Alice, CLAIM_AMOUNT, aliceProof, v, r, s);
     }
 
-    function testBobCanClaim() public {
+    function testMultipleClaim() public {
         uint256 initBobBal = token.balanceOf(Bob);
-        assert(initBobBal == 0);
+        uint256 initClaraBal = token.balanceOf(Clara);
+        uint256 initDanBal = token.balanceOf(Dan);
         uint256 airdropBal = token.balanceOf(address(airdrop));
+
+        assert(initBobBal == 0);
+        assert(initClaraBal == 0);
+        assert(initDanBal == 0);
         assert(airdropBal == SEND_AMOUNT);
 
         bytes32 bobDigest = airdrop.getMessageHash(Bob, CLAIM_AMOUNT);
-        (uint8 v, bytes32 r, bytes32 s) = vm.sign(bobPrvKey, bobDigest);
+        bytes32 claraDigest = airdrop.getMessageHash(Clara, CLAIM_AMOUNT);
+        bytes32 danDigest = airdrop.getMessageHash(Dan, CLAIM_AMOUNT);
 
+        // sign messages with respective  user private key
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(bobPrvKey, bobDigest);
+        (uint8 a, bytes32 b, bytes32 c) = vm.sign(claraPrvKey, claraDigest);
+        (uint8 x, bytes32 y, bytes32 z) = vm.sign(danPrvKey, danDigest);
+
+        // claim
+        vm.startPrank(gasPayer);
+
+        // Bob
         airdrop.claimHugs(Bob, CLAIM_AMOUNT, bobProof, v, r, s);
         uint256 finalBobBal = token.balanceOf(Bob);
         uint256 airdropBalAfterBobClaim = token.balanceOf(address(airdrop));
         assert(finalBobBal == CLAIM_AMOUNT);
         assert(airdropBalAfterBobClaim == 75e18);
+
+        // Clara
+        airdrop.claimHugs(Clara, CLAIM_AMOUNT, claraProof, a, b, c);
+        uint256 finalClaraBal = token.balanceOf(Clara);
+        uint256 airdropBalAfterClaraClaim = token.balanceOf(address(airdrop));
+        assert(finalClaraBal == CLAIM_AMOUNT);
+        assert(airdropBalAfterClaraClaim == 50e18);
+
+        // Dan
+        airdrop.claimHugs(Dan, CLAIM_AMOUNT, danProof, x, y, z);
+        uint256 finalDanBal = token.balanceOf(Dan);
+        uint256 airdropBalAfterDanClaim = token.balanceOf(address(airdrop));
+        assert(finalDanBal == CLAIM_AMOUNT);
+        assert(airdropBalAfterDanClaim == 25e18);
     }
 
-    // function testMultipleClaim() public {
-    //     uint256 initBobBal = token.balanceOf(Bob);
-    //     uint256 initClaraBal = token.balanceOf(Clara);
-    //     uint256 initDanBal = token.balanceOf(Dan);
-    //     uint256 airdropBal = token.balanceOf(address(airdrop));
+    function testFailWhenRepeatClaim() public {
+        uint256 initAliceBal = token.balanceOf(Alice);
+        uint256 airdropBalBeforeAliceFirstClaim = token.balanceOf(address(airdrop));
 
-    //     bytes32 proofA = 0xb4ad04bfcc1a89a0939f16e689fd17d2435174d8431a765e538b306bebd0438e;
-    //     bytes32 proofB = 0x543793e984215b0a24b11f843eea8755b11141397ef712e5af3111067a3019a8;
-    //     bytes32[2] memory bobProof;
+        assertEq(initAliceBal, 0);
+        assertEq(airdropBalBeforeAliceFirstClaim, SEND_AMOUNT);
 
-    //     assert(initBobBal == 0);
-    //     assert(initClaraBal == 0);
-    //     assert(initDanBal == 0);
-    //     assert(airdropBal == SEND_AMOUNT);
+        bytes32 digest = airdrop.getMessageHash(Alice, CLAIM_AMOUNT);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(alicePrvKey, digest);
 
-    //     bytes32 bobDigest = airdrop.getMessageHash(Bob, CLAIM_AMOUNT);
-    //     bytes32 claraDigest = airdrop.getMessageHash(Clara, CLAIM_AMOUNT);
-    //     bytes32 danDigest = airdrop.getMessageHash(Dan, CLAIM_AMOUNT);
+        vm.prank(gasPayer);
+        airdrop.claimHugs(Alice, CLAIM_AMOUNT, aliceProof, v, r, s);
+        uint256 finalAliceBal = token.balanceOf(Alice);
+        uint256 airdropBalAfterAliceSuccessClaim = token.balanceOf(address(airdrop));
 
-    //     // sign messages with respective  user private key
-    //     (uint8 v, bytes32 r, bytes32 s) = vm.sign(bobPrvKey, bobDigest);
-    //     (uint8 a, bytes32 b, bytes32 c) = vm.sign(claraPrvKey, claraDigest);
-    //     (uint8 x, bytes32 y, bytes32 z) = vm.sign(danPrvKey, danDigest);
+        assert(finalAliceBal > initAliceBal);
+        assert(finalAliceBal == CLAIM_AMOUNT);
+        assert(airdropBalBeforeAliceFirstClaim > airdropBalAfterAliceSuccessClaim);
+        assertEq(airdropBalBeforeAliceFirstClaim - airdropBalAfterAliceSuccessClaim, CLAIM_AMOUNT);
 
-    //     // claim
-    //     vm.startPrank(gasPayer);
+        // Try to claim again
+        vm.prank(gasPayer);
+        vm.expectRevert(HugsAirdrop.HugsAirdrop__AlreadyClaimedHugs.selector);
+        airdrop.claimHugs(Alice, CLAIM_AMOUNT, aliceProof, v, r, s);
 
-    //     // Bob
-    //     airdrop.claimHugs(Bob, CLAIM_AMOUNT, PROOF, v, r, s);
-    //     uint256 finalBobBal = token.balanceOf(Bob);
-    //     uint256 airdropBalAfterBobClaim = token.balanceOf(address(airdrop));
-    //     assert(finalBobBal == CLAIM_AMOUNT);
-    //     assert(airdropBalAfterBobClaim == 75e18);
-    // }
+        uint256 balOfAliceAfterFailClaim = token.balanceOf(Alice);
+        uint256 airdropBalAfterAliceFailClaim = token.balanceOf(address(airdrop));
+
+        assert(balOfAliceAfterFailClaim == finalAliceBal);
+        assert(airdropBalAfterAliceSuccessClaim == airdropBalAfterAliceFailClaim);
+    }
 }
