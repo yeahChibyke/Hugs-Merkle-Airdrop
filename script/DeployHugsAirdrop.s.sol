70278f580894a7f10391d3512e22c8443904731d
yeahChibyke <chibykepride@gmail.com>
2024-09-17 03:29:40 +0100

    will figure out interactions script later on

--- script/DeployHugsAirdrop.s.sol
+++ script/DeployHugsAirdrop.s.sol
@@ -7,7 +7,8 @@ import {Script} from "forge-std/Script.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract DeployHugsAirdrop is Script {
-    bytes32 private s_merkleRoot = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
+    // bytes32 private s_merkleRoot = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
+    bytes32 private s_merkleRoot = 0xb1155292a7f465f73a70be41a25a6d7ab8e760cabb68c08072856aa05eb23e32;
     uint256 constant MINT_AMOUNT = 100e18;
 
     function deployHugsAirdrop() public returns (HugsAirdrop, HugsToken) {
---
+++ script/Interactions.s.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {Script, console2} from "forge-std/Script.sol";
+import {DevOpsTools} from "Foundry-Devops/src/DevOpsTools.sol";
+import {HugsAirdrop} from "../src/HugsAirdrop.sol";
+
+contract ClaimAirdrop is Script {
+// address account = 0x4dBa461cA9342F4A6Cf942aBd7eacf8AE259108C;
+// uint256 amount = 25e18;
+// bytes32 proofOne = 0xb4ad04bfcc1a89a0939f16e689fd17d2435174d8431a765e538b306bebd0438e;
+// bytes32 proofTwo = 0x543793e984215b0a24b11f843eea8755b11141397ef712e5af3111067a3019a8;
+// bytes32[] proof = [proofOne, proofTwo];
+// function claimAirdrop(address airdrop) public {
+//     vm.startBroadcast();
+//     HugsAirdrop(airdrop).claimHugs(account, amount, proof, v, r, s);
+//     vm.stopBroadcast();
+// }
+
+// function run() external {
+//     address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("HugsAirdrop", block.chainid);
+//     claimAirdrop(mostRecentlyDeployed);
+// }
+}
+// will figure you out later on. Overall project is good to go
